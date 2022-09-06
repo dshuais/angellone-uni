6ef2b34c4fffe9452626108a149a6a99ba258e27
dushuais <1137896420@qq.com>
2022-09-06 11:46:26 +0800

    改main公共样式布局

--- pages/noneNetwork/index.vue
+++ pages/noneNetwork/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="container">
-		<view class="main">
+		<view class="main2">
 			<u--image :src="require('../../static/image/none.png')" width="400rpx" height="400rpx"></u--image>
 			<p class='c-999 mb20'>世界上最远的距离就是断网...</p>
 			<u-button text="重新进入" color='#6C63FF' @click='handelReload'></u-button>
@@ -30,7 +30,8 @@
 
 <style lang="scss" scoped>
 .container {
-	.main {
+	background: #fff;
+	.main2 {
 		transform: translateX(-50%);
 		margin: 20% 0 0 50%;
 		text-align: center;
